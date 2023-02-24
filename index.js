716530ac23b2aee5f2df90054ff2891e9f327a1c
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-24 18:19:06 +0000

    Enquirer

--- index.js
+++ index.js
@@ -10,47 +10,117 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer.prompt([{
-    //managerquestions
-}]).then(response => {
-    // populate manager info
-    // promptForNexEMployee ()
-})
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Add the name of the team Manager:\n >> ",
+      validate(answer) {
+        if (answer === "") {
+          return "You must add the team Manager name!";
+        }
+        return true;
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Add the Manager ID:\n >> ",
+      validate(answer){
+        if (answer === ""){
+          return "You must add the Manager ID!"
+        }
+        return true;
+      }
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Add the Manager Email:\n >> ",
+      validate(answer){
+        if (answer === ""){
+          return "You must add the Manager Email!"
+        }
+        return true;
+      }
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Add the Office Number:\n >> ",
+      validate(answer){
+        if (answer === ""){
+          return "You must add the Office Number!"
+        }
+        return true;
+      }
+    },
+  ])
+  .then((resp) => {
+    console.log(resp);
+    const manager = new Manager(resp.name, resp.id, resp.email, resp.officeNumber)
+    console.log(manager);
+    promptForNextEmployee()
+  });
 
 const promptForNextEmployee = () => {
-    inquirer.prompt([{
-        // choice of 3
-    }]).then(response => {
-        // if engineer
-        //    promptForEngineer
-        // else if intern
-        //    promptForIntern
-        // else
-        //    use the functionality from page-template to generate the team
-    })
-}
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employee",
+        message: "Which position do you want for your team?",
+        choices: ["Engineer", "Intern", "The team doesn't need anyone else."],
+        filter(val){
+          return val.toLowerCase();
+        }
+      },
+    ])
+    .then((resp) => {
+      if(resp.engineer){
+        promptForEngineer()
+      } else if(resp.intern){
+        promptForIntern()
+      } else {
+        buildPage()
+      }
+
+      // if engineer
+      //    promptForEngineer
+      // else if intern
+      //    promptForIntern
+      // else
+      //    use the functionality from page-template to generate the team
+    });
+};
 
 const promptForEngineer = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         //engineer questions
-    }]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
-    })
-}
+      },
+    ])
+    .then((response) => {
+      // add new engineer to employees array
+      // promptForNextEmployee
+    });
+};
 
 const promptForIntern = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         //intern questions
-    }]).then(response => {
-        // add new intern to employees array
-        // promptForNextEmployee
-    })
-}
+      },
+    ])
+    .then((response) => {
+      // add new intern to employees array
+      // promptForNextEmployee
+    });
+};
 
-const buildPage = () => {
-
-}
+const buildPage = () => {};
